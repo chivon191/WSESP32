91ce70e8a26a852e6b2e10129eedd7533bb6ca4d
Von Nguyen Chi <intern.ncvon@tma.com.vn>
2024-10-29 15:28:44 +0700

    fingerprint change

--- test-sensor/AS608/AS608.ino
+++ test-sensor/AS608/AS608.ino
@@ -2,6 +2,7 @@
 #define LED 2
 #define RXD2 16   // Chân RX của ESP32 (kết nối với TX của AS608)
 #define TXD2 17   // Chân TX của ESP32 (kết nối với RX của AS608)
+#define tchFinger 19
 
 HardwareSerial mySerial(2);  // Serial2 sử dụng TXD2 và RXD2
 Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
@@ -11,17 +12,16 @@ uint8_t id;
 void setup() {
   Serial.begin(115200);
   pinMode(LED, OUTPUT);
+  pinMode(tchFinger, INPUT);
   digitalWrite(LED, LOW);
   mySerial.begin(57600, SERIAL_8N1, RXD2, TXD2);
-
-  Serial.println("\n\nAS608 Fingerprint sensor with add/delete/check");
 
   // Khởi động cảm biến vân tay
   finger.begin(57600);
   if (finger.verifyPassword()) {
     Serial.println("Cảm biến vân tay đã kết nối thành công!");
   } else {
-    Serial.println("Không tìm thấy cảm biến vân tay :(");
+    Serial.println("Không tìm thấy cảm biến vân tay!");
     while (1) { delay(1); }
   }
 }
@@ -48,71 +48,78 @@ void loop() {
   }
 }
 
-void addFingerprint() {
-  Serial.println("Nhập ID vân tay muốn lưu (1 - 127):");
-  id = readNumber();
-  if (id == 0) return;
-
-  Serial.print("Đang thêm vân tay ID #");
-  Serial.println(id);
-
-  if (getFingerprintEnroll()) {
-    Serial.println("Đã lưu vân tay thành công!");
-  } else {
-    Serial.println("Lưu vân tay thất bại!");
-  }
-
-  // Quay lại menu chính
-  Serial.println("Quay lại menu chính...");
-}
-
-void deleteFingerprint() {
-  Serial.println("Nhập ID vân tay muốn xóa (1 - 127):");
-  id = readNumber();
-  if (id == 0) return;
-
-  if (finger.deleteModel(id) == FINGERPRINT_OK) {
-    Serial.println("Đã xóa vân tay thành công!");
-  } else {
-    Serial.println("Xóa vân tay thất bại!");
-  }
-}
-
-void checkFingerprint() {
-  Serial.println("Đặt ngón tay lên cảm biến để kiểm tra...");
-  uint8_t p = finger.getImage();
-  if (p == FINGERPRINT_OK) {
-    p = finger.image2Tz();
-    if (p == FINGERPRINT_OK) {
-      p = finger.fingerFastSearch();
-      if (p == FINGERPRINT_OK) {
-        Serial.print("Đã nhận diện thành công ID #");
-        Serial.print(finger.fingerID);
-        Serial.print(" với độ tin cậy: ");
-        Serial.println(finger.confidence);
-        digitalWrite(LED, HIGH);
-        delay(2000);
-        digitalWrite(LED, LOW);
+bool deleteFingerprint() {
+  Serial.println("Vui lòng scan vân tay muốn xóa...");
+  for (int i=0; i<3; i++) {
+    while(!digitalRead(tchFinger)) {
+      Serial.print("Đặt tay lên cảm biến....");
+      delay(500);
+    }
+    if (finger.getImage() == FINGERPRINT_OK) {
+      if (finger.image2Tz() == FINGERPRINT_OK) {
+        if (finger.fingerFastSearch() == FINGERPRINT_OK) {
+          id = finger.fingerID;
+          if (finger.deleteModel(id) == FINGERPRINT_OK) {
+            Serial.println("Đã xóa vân tay thành công!");
+            return true;
+          } else {
+            Serial.println("Xóa vân tay thất bại");
+          }
+        } else {
+          Serial.println("Không tìm thấy vân tay cần xóa");
+        }
       } else {
-        Serial.println("Không tìm thấy vân tay!");
+        Serial.println("Không thể chuyển đổi hình ảnh vân tay");
       }
     } else {
-      Serial.println("Không chuyển đổi được hình ảnh vân tay.");
+      Serial.println("Không tìm thấy vân tay.");
     }
-  } else if (p == FINGERPRINT_NOFINGER) {
-    // Không có vân tay, có thể bỏ qua
-  } else {
-    Serial.println("Lỗi không xác định.");
   }
+  return false;
 }
 
-uint8_t getFingerprintEnroll() {
+bool checkFingerprint() {
+  Serial.println("Đặt ngón tay lên cảm biến để kiểm tra...");
+  for (int i=0; i<3; i++) {
+    while(!digitalRead(tchFinger)) {
+      Serial.print("Đặt tay lên cảm biến....");
+      delay(500);
+    }
+    if (finger.getImage() == FINGERPRINT_OK) {
+      if (finger.image2Tz() == FINGERPRINT_OK) {
+        if (finger.fingerFastSearch() == FINGERPRINT_OK) {
+          Serial.print("Xác thực vân tay thành công với ID ");
+          Serial.println(finger.fingerID);
+          return true;
+        } else {
+          Serial.println("Không tìm thấy vân tay.");
+        }
+      } else {
+        Serial.println("Không thể chuyển đổi hình ảnh vân tay");
+      }
+    } else {
+      Serial.println("Không tìm thấy vân tay.");
+    }
+  }
+  return false;
+}
+
+int getNextAvailableID() {
+  for (int i = 1; i < 127; i++)
+    if (finger.loadModel(i) != FINGERPRINT_OK) 
+      return i;
+  return 0;
+}
+
+bool getFingerprintEnroll() {
   int p = -1;
-  Serial.println("Đặt ngón tay lên cảm biến...");
+  while(!digitalRead(tchFinger)) {
+      Serial.print("Đặt tay lên cảm biến....");
+      delay(500);
+  }
   while (p != FINGERPRINT_OK) {
     p = finger.getImage();
     if (p == FINGERPRINT_NOFINGER) {
-      // Đợi người dùng đặt ngón tay lên cảm biến
       delay(500); // Đợi một chút trước khi kiểm tra lại
     } else if (p == FINGERPRINT_IMAGEFAIL) {
       Serial.println("Lỗi chụp hình ảnh");
@@ -123,24 +130,21 @@ uint8_t getFingerprintEnroll() {
     }
   }
 
-  p = finger.image2Tz(1);
-  if (p != FINGERPRINT_OK) {
+  if (finger.image2Tz(1) != FINGERPRINT_OK) {
     Serial.println("Lỗi chuyển đổi hình ảnh");
-    return p;
+    return false;
   }
-
   Serial.println("Lấy dấu vân tay lần 1 thành công. Hãy tháo tay.");
   delay(2000);
-  while (p != FINGERPRINT_NOFINGER) {
-    p = finger.getImage();
-  }
 
-  Serial.println("Đặt lại ngón tay lần nữa...");
+  while(!digitalRead(tchFinger)) {
+      Serial.print("Đặt tay lên cảm biến để lấy dấu lần 2...");
+      delay(500);
+  }
   p = -1;
   while (p != FINGERPRINT_OK) {
     p = finger.getImage();
     if (p == FINGERPRINT_NOFINGER) {
-      // Đợi người dùng đặt ngón tay lên cảm biến
       delay(500); // Đợi một chút trước khi kiểm tra lại
     } else if (p == FINGERPRINT_IMAGEFAIL) {
       Serial.println("Lỗi chụp hình ảnh");
@@ -151,24 +155,26 @@ uint8_t getFingerprintEnroll() {
     }
   }
 
-  p = finger.image2Tz(2);
-  if (p != FINGERPRINT_OK) {
+  if (finger.image2Tz(2) != FINGERPRINT_OK) {
     Serial.println("Lỗi chuyển đổi hình ảnh lần 2");
-    return p;
+    return false;
   }
 
-  p = finger.createModel();
-  if (p != FINGERPRINT_OK) {
-    Serial.println("Không khớp hai lần quét vân tay");
-    return p;
+  if (finger.createModel() == FINGERPRINT_OK) {
+    if(finger.fingerFastSearch() == FINGERPRINT_OK) {
+      Serial.println("Trùng với vân tay đã đăng ký!");
+    }
+    else {
+      id = getNextAvailableID();
+      finger.storeModel(id);
+      Serial.print("Đăng ký vân tay thành công với ID ");
+      Serial.println(id);
+      return true;
+    }
   }
-
-  p = finger.storeModel(id);
-  if (p == FINGERPRINT_OK) {
-    return true; // Trả về true nếu lưu thành công
-  } else {
-    Serial.println("Lưu vân tay thất bại.");
-    return false; // Trả về false nếu lưu thất bại
+  else {
+    Serial.println("Đăng ký vân tay thất bại.");
+    return false;
   }
 }
 
